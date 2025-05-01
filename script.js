22fe2f9754f9f45506743cf6be42c2a1fdba6cc4
Ali Ghaffari <aligh71@yahoo.com>
2025-05-01 17:53:23 +0330

    first commit of my website

---
+++ script.js
@@ -0,0 +1,7 @@
+// const ch17JsTitle = document.querySelector("#ch17javascript");
+// const ch17JsBody = document.querySelector(".ch17javascript");
+
+// ch17JsTitle.addEventListener("click", function () {
+//   ch17JsBody.style.display = "block";
+//   console.log(ch17JsBody);
+// });
